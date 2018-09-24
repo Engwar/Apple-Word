e4a25b4eedc0520267089de90020e0f9f3e6b4f3
Igor Shelginskiy <engwar@macbook-pro-igor.local>
2018-09-24 16:18:00 +0300

    Created game struct and update UI

---
+++ Apple Word/Game.swift
@@ -0,0 +1,17 @@
+//
+//  Game.swift
+//  Apple Word
+//
+//  Created by Igor Shelginskiy on 24.09.2018.
+//  Copyright © 2018 Igor Shelginskiy. All rights reserved.
+//
+
+import Foundation
+
+struct Game {
+    // duessed word
+    var word: String
+    
+    // amount of attempts
+    var incorrectMovesRemaining: Int
+}
--- Apple Word/ViewController.swift
+++ Apple Word/ViewController.swift
@@ -10,26 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    //list of words for guessing
+    var listOfWords = ["вишня", "апельсин", "груша", "нектарин", "слива",]
+    
+    // max of number of attempts
+    let incorrectMovesAllowed = 7
+    
+    //amounts of wins and loses
+    var totalWins = 0
+    var totalLosses = 0
+    
+    var currentGame: Game!
+    
     @IBOutlet weak var treeImageView: UIImageView!
-    
     @IBOutlet var letterButtons: [UIButton]!
-    
-    
     @IBOutlet weak var correctWordLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //start of new round of game
+        newRound()
+    }
+    
+    //start of new round of game
+    func newRound() {
+        let newWord = listOfWords.removeFirst()
         
-        for button in letterButtons {
-           _ = button
-        }
+        currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed )
+    }
+    
+    //update of interface
+    func updateUI() {
+        //update pictures
+        let imageName = "Tree \(currentGame.incorrectMovesRemaining)"
+        let image = UIImage(named: imageName)
+        treeImageView.image = image
+        
+        //update score
+        scoreLabel.text = "Wins: \(totalWins), Losses: \(totalLosses)"
     }
     
     @IBAction func buttonTapped(_ sender: UIButton) {
         sender.isEnabled = false
     }
-    
 
 }
 
